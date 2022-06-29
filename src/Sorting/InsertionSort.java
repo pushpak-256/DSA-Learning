be0f9bde86c8ef068d8fac19fc5337edc8e9d4cd
Pushpak Bhagat <pbhagat@montran.com>
2022-06-29 11:30:33 +0530

    :package: Insertion Sort

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -16,13 +16,21 @@ public class InsertionSort {
 
 	public static void insertionSort(int[] arr) {
 		
+		
+		
 		int firstIndexOfUnsorted = 1;  // assume 0 element is sorted part
 		
 		for (; firstIndexOfUnsorted < arr.length; firstIndexOfUnsorted++) {
 
 			int curr = arr[firstIndexOfUnsorted];
-			int j = firstIndexOfUnsorted;
-			for (; j > 0 && arr[j - 1] > curr; j--) { // if curr < curr-1 , then only swap,
+		
+			
+			int insert =binarySerach(arr,curr,0,-1);
+			int temp = arr[insert];
+			
+			int j = insert;
+			for (;j<firstIndexOfUnsorted; j++) { // if curr < curr-1 , then only swap,
+			
 				arr[j] = arr[j- 1];
 			}
 			arr[j] = curr; // j is the Insertion position for curr element
@@ -30,6 +38,18 @@ public class InsertionSort {
 		}
 		
 	}
+	
+	public static int binarySerach(int nums [] , int  curr, int low , int high) {
+	   int mid = 0;
+		while(low<=high) {
+			mid = (low+high)>>>1;
+			if(nums[mid]==curr) {high=mid-1;}
+			else if(nums[mid]>curr) {high=mid-1;}
+			else low =mid+1;
+		}
+		return low;
+	}
+	
 	public static void main(String[] args) {
 		int[] arr = new int[] { 4, 1, 3, 9, 7 };
 		insertionSort(arr);
